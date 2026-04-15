7a86bb5f5e633a13d4069e323f9dba16c0ed575b
Soumadip Das <soumadip@zohomail.in>
2026-04-15 13:22:30 +0530

    cleanup: remove components as it's now implemented in portosaurus

--- src/theme/MDXComponents.js
+++
@@ -1,9 +0,0 @@
-import MDXComponents from '@theme-original/MDXComponents';
-import SourcePreview from '@site/src/components/SourcePreview';
-import Details from '@theme/Details';
-
-export default {
-  ...MDXComponents,
-  SourcePreview,
-  Details,
-};
