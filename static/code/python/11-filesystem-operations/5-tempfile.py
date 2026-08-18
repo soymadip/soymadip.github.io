4f91875574525a640097dc51919ff0d227df6864
Soumadip Das <soumadip+github@zohomail.in>
2026-08-18 10:55:02 +0530

    code: update lessons

--- static/code/python/11-filesystem-operations/5-tempfile.py
+++ static/code/python/11-filesystem-operations/5-tempfile.py
@@ -1,10 +1,31 @@
 # tempfie is for creating temporary files and dirs safely
+#
 
 from pathlib import Path
-from tempfile import NamedTemporaryFile, TemporaryDirectory, TemporaryFile, gettempdir
+from tempfile import (
+    NamedTemporaryFile,
+    TemporaryDirectory,
+    TemporaryFile,
+    gettempdir,
+    mkdtemp,
+    mkstemp,
+)
 
 # Why not just mkdir?
 Path("/tmp/my/tmp_file").mkdir()
+
+"""We don't know:
+
+whether it already exists
+whether another process created it
+whether someone can interfere with it
+whether the name is predictable
+
+tempfile handles secure temporary-name generation.
+That's one of the main reasons it exists.
+
+"""
+
 
 # This can have name collisions & security issues
 # instead use tempfile
@@ -32,3 +53,12 @@ with NamedTemporaryFile() as file:
 # ------- gettempdir() - get the temporary directory --------
 #
 print(gettempdir())  # returns path as string
+
+
+# Low level api:
+
+dir = mkdtemp()  # Make temporary dir. accepts optional prefix (filename)
+id, path = mkstemp()  # Make temporary file. returns id and path as strings
+
+print(dir)
+print(id, path)
