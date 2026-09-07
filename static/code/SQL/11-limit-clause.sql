8bf9ede057dba395b4958fc8372a90563007764a
Soumadip Das <soumadip+github@zohomail.in>
2026-09-07 19:54:45 +0530

    sql: add offset to limit

--- static/code/SQL/11-limit-clause.sql
+++ static/code/SQL/11-limit-clause.sql
@@ -10,3 +10,12 @@ SELECT * FROM student LIMIT 5;
 -- Eg: list 4 students whose marks > 56
 SELECT * FROM student WHERE marks > 56 LIMIT 5;
 
+
+-- --------------------- Setting Offset --------------------
+
+-- We use OFFSET option to set an offset.
+-- generally this is used with order by clause to skip rows.
+
+-- List second and third highest marks of students
+SELECT * FROM student ORDER BY grade DESC LIMIT 2 OFFSET 1; -- skips 1st result
+
