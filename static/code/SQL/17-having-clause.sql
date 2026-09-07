4af8a81f5fde1816ac9712e187aa326918926fdd
Soumadip Das <soumadip+github@zohomail.in>
2026-09-07 23:58:55 +0530

    chore: update lock

--- static/code/SQL/17-having-clause.sql
+++ static/code/SQL/17-having-clause.sql
@@ -3,5 +3,4 @@
 -- Similar to WHERE caluse, aplies some conditions to rows.
 -- But when we want to apply any condition AFTER GROUPING
 
-
 SELECT city, COUNT(rollno) FROM student GROUP BY city HAVING MAX(marks) > 90;
