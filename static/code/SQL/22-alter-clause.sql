260e5ceb3fba6220286580fc14fdeeaf4aab6507
Soumadip Das <soumadip+github@zohomail.in>
2026-09-07 14:36:07 +0530

    learn alter clause

---
+++ static/code/SQL/22-alter-clause.sql
@@ -0,0 +1,69 @@
+-- ==================== ALTER Clause ====================
+
+-- ALTER TABLE statement is used to change the schema of an existing table.
+-- It can be used to add, drop, or modify columns in an existing table.
+
+
+-- Syntax: ALTER TABLE table_name action;
+
+-- Action can be:
+
+
+-- --------------- ADD COLUMN -----------------
+
+-- Add a new column to the table.
+
+ALTER TABLE table_name ADD COLUMN column_name datatype constraints;
+
+
+-- --------------- DROP COLUMN -----------------
+
+-- Drop a column from the table.
+
+ALTER TABLE table_name DROP COLUMN column_name;
+
+
+-- --------------- MODIFY COLUMN -----------------
+
+-- Modify the definition of an existing column.
+
+ALTER TABLE table_name MODIFY COLUMN column_name new_datatype constraints;
+
+
+-- --------------- RENAME COLUMN -----------------
+
+-- Rename a column in the table.
+
+ALTER TABLE table_name RENAME COLUMN old_column_name TO new_column_name;
+
+
+-- --------------- RENAME TO -----------------
+
+-- Rename a table.
+
+ALTER TABLE table_name RENAME TO new_table_name;
+
+
+-- --------------- CHANGE COLUMN -----------------
+
+-- Change the name and/or definition of an existing column.
+
+ALTER TABLE table_name CHANGE COLUMN old_column_name new_column_name new_datatype constraints;
+
+
+
+
+
+
+-- --------- Ex ------------
+
+-- Add age column to student table
+ALTER TABLE student ADD COLUMN age INT;  -- New column's values will be NULL by default
+
+
+-- Drop age column from student table
+ALTER TABLE student DROP COLUMN age;
+
+-- Now marks column can not have NULL values
+ALTER TABLE student MODIFY COLUMN marks INT NOT NULL;
+
