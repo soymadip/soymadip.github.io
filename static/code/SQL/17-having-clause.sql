723e9f3b2b404f503c36500495b93fd613fb2ecb
Soumadip Das <soumadip+github@zohomail.in>
2026-09-16 10:00:22 +0530

    sql: clearify having usage

--- static/code/SQL/17-having-clause.sql
+++ static/code/SQL/17-having-clause.sql
@@ -3,4 +3,6 @@
 -- Similar to WHERE caluse, aplies some conditions to rows.
 -- But when we want to apply any condition AFTER GROUPING
 
+-- HAVING IS APLIED ONCE PER GROUP. whereas WHRE is applied to each row.
+
 SELECT city, COUNT(rollno) FROM student GROUP BY city HAVING MAX(marks) > 90;
